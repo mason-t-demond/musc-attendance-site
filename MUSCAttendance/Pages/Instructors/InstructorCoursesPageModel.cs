e6699164b5e1b7cec06c28f8b4c36572ed896cd7
elliotallen1 <allened@hendrix.edu>
2023-11-06 15:10:43 -0600

    Restructured Project Website

---
+++ MUSCAttendance/Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,32 @@
+using MUSCAttendance.Data;
+using MUSCAttendance.Models;
+using MUSCAttendance.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MUSCAttendance.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseData> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context,
+                                               Instructor instructor)
+        {
+            var allCourses = context.Courses;
+            var instructorCourses = new HashSet<int>(
+                instructor.Courses.Select(c => c.CourseID));
+            AssignedCourseDataList = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+    }
+}
