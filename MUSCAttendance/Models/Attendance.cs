d53e3279ad2064d5a4a180691fc9e702c540d1d1
Ryan Fuller <fullerra@hendrix.edu>
2023-11-14 18:54:48 -0600

    Created Student and Attendance Classes

--- MUSCAttendance/Models/Attendance.cs
+++ MUSCAttendance/Models/Attendance.cs
@@ -1,18 +1,18 @@
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MUSCAttendance.Models
 {
+    public enum Type
+    {
+        Hendrix, UCA, Other
+    }
+
     public class Attendance
     {
-        public int AttendanceID { get; set; }
-        public int CourseID { get; set; }
         public int StudentID { get; set; }
 
         [Display(Name = "Event Type")]
-        public EventType EventType { get; set; }
-
-        public Course Course { get; set; }
+        public Type type { get; set; }
         public Student Student { get; set; }
     }
 }
--- MUSCAttendance/Models/Student.cs
+++ MUSCAttendance/Models/Student.cs
@@ -1,45 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using MUSCAttendance.Pages.Courses;
-using MUSCAttendance.Pages.Instructors;
-
 namespace MUSCAttendance.Models
 {
     public class Student
     {
         public int ID { get; set; }
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
-
-
-        [Required]
-        [Display(Name = "Graduation Year")]
-        public int GraduationYear { get; set; }
-
-        [Required]
-        [Display(Name = "Total Attendances")]
-        public int TotalAttendances { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
+        public DateTime GradYear { get; set; }
 
         public ICollection<Attendance> Attendances { get; set; }
-
-        public List<Course> Courses { get; set; }
     }
 }
