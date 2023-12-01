b3e92e06aa7f31273edb97b6dd7af8d68c5cb359
Ryan Fuller <fullerra@hendrix.edu>
2023-12-01 13:57:52 -0600

    Added Identity Framework
    Still need to implement it, but this is a checkpoint in case I accidentally create two contexts!

--- MUSCAttendance/obj/Debug/net7.0/MUSCAttendance.AssemblyInfo.cs
+++ MUSCAttendance/obj/Debug/net7.0/MUSCAttendance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc53e318f650f626b4aa956f55b56cd7840c3a32")]
 [assembly: System.Reflection.AssemblyProductAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
