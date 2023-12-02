ed89fd33bd2781b8052d1c74b5d11cc87c7e8a70
Ryan Fuller <fullerra@hendrix.edu>
2023-12-01 20:51:36 -0600

    Basic Log-In Implemented

--- MUSCAttendance/obj/Debug/net7.0/MUSCAttendance.AssemblyInfo.cs
+++ MUSCAttendance/obj/Debug/net7.0/MUSCAttendance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6becf4ee237646ab770a2c09f70cf27105920285")]
 [assembly: System.Reflection.AssemblyProductAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
