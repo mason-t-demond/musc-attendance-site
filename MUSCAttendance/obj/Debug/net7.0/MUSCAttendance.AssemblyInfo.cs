156322631b344d13b1356a4b9e20b757b7b337f3
Ryan Fuller <fullerra@hendrix.edu>
2023-12-08 14:11:34 -0600

    Added back the Create New Student Button

--- MUSCAttendance/obj/Debug/net7.0/MUSCAttendance.AssemblyInfo.cs
+++ MUSCAttendance/obj/Debug/net7.0/MUSCAttendance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb174af705ae3793e10c4cf8c119723dc29066d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MUSCAttendance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
