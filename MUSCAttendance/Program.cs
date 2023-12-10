5bb9362e297ad1e5293f13b5f216932e6c9d7763
elliotallen1 <allened@hendrix.edu>
2023-12-10 15:35:13 -0600

    LogIn Button is Orange
    Log in button is orange

--- MUSCAttendance/Program.cs
+++ MUSCAttendance/Program.cs
@@ -2,7 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MUSCAttendance.Data;
 using Microsoft.AspNetCore.Identity;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
