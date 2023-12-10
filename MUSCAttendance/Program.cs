503f6a2028831196bee642efdef32382ba5faa2d
Tyrone Mason <masonmillionaire725@gmail.com>
2023-12-09 18:36:36 -0600

    Edited appsettings and Program.cs to include AZURE_SQL_CONNECTIONSTRING

--- MUSCAttendance/Program.cs
+++ MUSCAttendance/Program.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
- builder.Services.AddDbContext<SchoolContext>(options =>
-     options.UseSqlite(builder.Configuration.GetConnectionString("SchoolContextSQLite") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
+builder.Services.AddDbContext<MyDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+builder.Services.AddDistributedMemoryCache();
 
  builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<SchoolContext>();
 
