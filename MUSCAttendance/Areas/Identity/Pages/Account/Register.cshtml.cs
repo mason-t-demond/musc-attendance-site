1f94a632667358df4c23c39c22981621becc2483
elliotallen1 <allened@hendrix.edu>
2023-12-10 21:27:30 -0600

    Another potential fix?

--- MUSCAttendance/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MUSCAttendance/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -134,16 +134,14 @@ namespace MUSCAttendance.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    // if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    // {
-                    //     return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    // }
-                    // else
-                    // {
-                    //     return RedirectToPage("Pages/Students/Create");
-
-                    // }
-                    return RedirectToPage("Pages/Students/Create");
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = "Pages/Students/Create" });
+                    }
+                    else
+                    {
+                        return RedirectToPage("Pages/Students/Create");
+                    }
 
                 }
                 foreach (var error in result.Errors)
